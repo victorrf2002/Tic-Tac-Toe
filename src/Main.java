b73674c08002fa4ea6b0db2ac7d3931dc418687e
victorrf <victorrf2002@gmail.com>
2024-04-11 00:21:22 -0400

    Wrote the printBoard() method that prints the game board in the console.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 public class Main {
+    static String[] board = new String[9];
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic-Tac-Toe");
         System.out.println("----------------------\n\n");
-        String[] board = new String[9];
+
+
+        //  Populating the board array
         for(int i = 0; i <9; i++) {
             board[i] = String.valueOf(i);
         }
@@ -12,7 +16,12 @@ public class Main {
 
     }
 
+    //  Method that prints the Tic-Tac-Toe game board
     public static void printBoard() {
-        System.out.println();
+        System.out.println("  " + board[0] + "  |  " + board[1] + "  |  " + board[2] );
+        System.out.println("- - -" + "|" + "- - -" + "|" + "- - -");
+        System.out.println("  " + board[3] + "  |  " + board[4] + "  |  " + board[5] );
+        System.out.println("- - -" + "|" + "- - -" + "|" + "- - -");
+        System.out.println("  " + board[6] + "  |  " + board[7] + "  |  " + board[8] );
     }
 }
