b6c0833c264e9db5b06ae9b53e2b15102f63cf79
victorrf <victorrf2002@gmail.com>
2024-05-08 14:52:46 -0400

    Imported java.util.Scanner and java.util.Random so that we can get input from both the user and the bot.
    Created method userTurn, which writes an 'X' on the square that the user chooses. Same thing applies to the method botTurn, except with an 'O'.
    
    Started writing the userValidity method that checks if the user's choice is valid.
    
    Notes for when I continue:
    - if user does not pass the userValidity check ask to re-enter the choice.
    - create botValidity method.
    - For both userValidity and botValidity, check if user or bot have already filled up a square. Because you can't choose a square that is already taken.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,38 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Main {
     static String[] board = new String[9];
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic-Tac-Toe");
-        System.out.println("----------------------\n\n");
-
+        System.out.println("----------------------\n");
+        System.out.println("Choose a square: \n");
 
         //  Populating the board array
-        for(int i = 0; i <9; i++) {
-            board[i] = String.valueOf(i);
+        for(int i = 0; i < 9; i++) {
+            board[i] = String.valueOf(i+1);
         }
 
         printBoard();
+
+        Scanner sc = new Scanner(System.in);
+        int userChoice = sc.nextInt();
+
+        userTurn(userChoice);
+        System.out.println("Player move: ");
+        printBoard();
+
+        Random rand = new Random();
+        System.out.println("Bot move: ");
+        int botChoice = rand.nextInt(10);
+
+        botTurn(botChoice);
+        printBoard();
+
+
+
+
+
 
     }
 
@@ -23,5 +43,28 @@ public class Main {
         System.out.println("  " + board[3] + "  |  " + board[4] + "  |  " + board[5] );
         System.out.println("- - -" + "|" + "- - -" + "|" + "- - -");
         System.out.println("  " + board[6] + "  |  " + board[7] + "  |  " + board[8] );
+    }
+
+    //  Method that plugs in X over the square the user chose
+    private static void userTurn(int userChoice) {
+        if(!userValidity(userChoice))
+            System.out.println("Error, not a valid square.");
+        else
+            board[userChoice - 1] = "X";
+    }
+
+    // Method that plugs in O over the square the bot randomly chose
+    private static void botTurn(int botChoice) {
+        board[botChoice - 1] = "O";
+    }
+
+    //  Method that checks if user choice is valid
+    private static boolean userValidity(int userChoice) {
+        if (userChoice != 1 && userChoice != 2 && userChoice != 3 && userChoice != 4 && userChoice != 5 &&
+                userChoice != 6 && userChoice != 7 && userChoice != 8 && userChoice != 9) {
+            return false;
+        }
+        else
+            return true;
     }
 }
